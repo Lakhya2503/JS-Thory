76b4f3eee567c8a6916cf82d7555e08b5fac888f
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-09 22:49:34 +0530

    Refine comments on type coercion example in 01.js

--- DSA/Basic/01.js
+++ DSA/Basic/01.js
@@ -2,7 +2,6 @@
 
 console.log("1" - 1) 
 // the "1" will convert into one of the substration operations that is called type cortion
-
-
-
+// also in *, / into
 //---------------------------------------------------//
+
