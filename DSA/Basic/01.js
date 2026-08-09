4db27d3b5c39ce290513b21a3cbf7edf63dba9c5
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-09 22:54:47 +0530

    Refactor comments for clarity in type coercion example in 01.js

--- DSA/Basic/01.js
+++ DSA/Basic/01.js
@@ -1,7 +1,10 @@
 // type Coercion
 
-console.log("1" - 1) 
+// console.log("1" - 1) 
 // the "1" will convert into one of the substration operations that is called type Coercion
 // also in *, / into
 //---------------------------------------------------//
 
+// let age = prompt("what is your age : ") use inspect mode use
+
+//---------------------------------------------------//
