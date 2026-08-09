df5b6538290c03c1b2d01dbd8ed7ace7dc565282
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-09 22:49:03 +0530

    Add initial implementation of type coercion example in 01.js

---
+++ DSA/Basic/01.js
@@ -0,0 +1,8 @@
+// type cortion
+
+console.log("1" - 1) 
+// the "1" will convert into one of the substration operations that is called type cortion
+
+
+
+//---------------------------------------------------//
