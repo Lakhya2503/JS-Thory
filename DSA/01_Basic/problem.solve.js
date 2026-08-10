a4356c2c589d623537fb02c521781713145a4782
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-10 17:32:17 +0530

    Refactor problem.solve.js to organize area calculations and improve readability

--- DSA/01_Basic/problem.solve.js
+++ DSA/01_Basic/problem.solve.js
@@ -14,3 +14,50 @@ console.log({otp})
 
 /* ========================================== */
 
+// area of parameter
+/*
+let length = 20;
+let height = 29;
+ 
+let areaParameter = length * height
+console.log({ areaParameter })
+
+
+/* ========================================== */
+
+// area of reactangle
+/*
+let length = 20;
+let height = 29;
+let areaReactangle = 2 * ( length + height )
+console.log({ areaReactangle })
+*/
+/* ========================================== */
+
+// area of triangle by hero's formula
+// formula \sqrt{s(s-a)(s-b)(s-c)}  hero's formula
+// s = \dfrac{(a + b + c)}{2} sideHalf formula
+/*
+let sideA = 15;
+let sideB = 20;
+let sideC = 10;
+
+let sideHalf = (sideA + sideB + sideC) / 2
+console.log({ sideHalf })
+let areaOfTriangle = Number(Math.floor(Math.sqrt( sideHalf * ( sideHalf - sideA ) * ( sideHalf - sideB ) * ( sideHalf - sideC ))))
+
+console.log({ areaOfTriangle })
+*/
+
+/* ========================================== */
+
+// area of circumfarnce
+/*
+let areaOfCircumfarnce;
+let pi = 3.14;
+let radius = 2;
+
+areaOfCircumfarnce = Number((pi * (Math.pow(radius, 2)).toFixed(2)))
+console.log({ areaOfCircumfarnce })
+*/
+/* ========================================== */
