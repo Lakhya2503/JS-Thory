95a88d723a51bfbd2176fea6d508c3024c10ab2a
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-10 14:13:58 +0530

    Add relational and logical operators sections in 02.js

--- DSA/Basic/02.js
+++ DSA/Basic/02.js
@@ -7,3 +7,11 @@ let ageOne = 35021;
 console.log(ageOne % 10) // 1
 console.log(Math.floor(ageOne / 10)) // 3502
 */
+
+
+// Relational Operators
+//  < , > , <=, =<, !=, !==
+
+// Logical Operator
+
+// &&, ||
