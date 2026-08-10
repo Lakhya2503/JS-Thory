a7d5909329090d65ae38ccf180cff22588659bbe
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-10 15:10:08 +0530

    Add missing section separators and improve formatting in 02.js

--- DSA/Basic/02.js
+++ DSA/Basic/02.js
@@ -8,10 +8,29 @@ console.log(ageOne % 10) // 1
 console.log(Math.floor(ageOne / 10)) // 3502
 */
 
+// --------------------------
 
 // Relational Operators
 //  < , > , <=, =<, !=, !==
 
+// --------------------------
+
 // Logical Operator
 
 // &&, ||
+
+// --------------------------
+
+// Unary Operator
+
+// ++, --
+
+// increment ( ++ )
+// post increment a++
+// pre increment ++a
+
+// decrement ( -- )
+// post decrement a--
+// pre decrement --a
+
+// --------------------------
