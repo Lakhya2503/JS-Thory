b9f31389633732b3f6c320b086317907bf647f18
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-10 17:00:55 +0530

    Add initial implementation of type coercion, arithmetic, relational, logical, unary operators, and math functions in new files

--- DSA/01_Basic/03.js
+++ DSA/01_Basic/03.js
@@ -1,5 +1,7 @@
 // Math 
+
 /* =================== */
+
 // Math.trunc()
 // remove the decimal like 95.04 it's remove the decimal 
 // part and show the output like 95 
@@ -9,8 +11,26 @@ let a = 20.574;
 let b = Math.trunc(a)
 console.log({b})
 */
+
 /* =================== */
+
 // Math.pow()
 // it taks two parameters he will increase the power like x * y 
 // like  y times x
 // ex.
+/*
+let mathPower = Math.pow(5,4) // it basically like 5 * 5 * 5 * 5 
+console.log({mathPower})
+*/
+
+/* =================== */
+
+// Math.random()
+
+// it will give a random number between 1 to 0
+// ex. 
+// console.log(Math.random())
+
+console.log(Math.trunc(Math.random() * 9000)+ 1000)
+
+/* =================== */
---
+++ DSA/01_Basic/problem.solve.js
@@ -0,0 +1,16 @@
+// OTP Generator function
+
+/* ========================================== */
+/*
+let otpGenerator = () => {
+    let otp;
+    otp = Math.floor((Math.random() * 9000) + 1000)
+    return otp 
+}
+
+let otp = otpGenerator()
+console.log({otp})
+*/
+
+/* ========================================== */
+
