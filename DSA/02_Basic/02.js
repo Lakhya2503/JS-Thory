0694a31899f66e7e4417cbebe95a234abd68d284
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-11 14:31:25 +0530

    Fix electricity bill calculation logic and improve output formatting

--- DSA/02_Basic/02.js
+++ DSA/02_Basic/02.js
@@ -13,23 +13,31 @@ let remaningUnit;
 let perUnitRate = 0;
 
 
-
 if(400 < electricityUnit) {
     unit = electricityUnit
     remaningUnit = unit - 400
-    electiryBill = remaningUnit * 20 
+    electiryBill = remaningUnit * 20 + ( 100 * 18 ) + ( 100 * 15 ) + ( 100 * 13 ) + ( 100 * 10 )
 } else if ( 400 >= electricityUnit && 301 >= electricityUnit ) {
     unit = electricityUnit
-
+    remaningUnit = unit - 300
+    electiryBill = remaningUnit * 18 + ( 100 * 15 ) + ( 100 * 13 ) + ( 100 * 10 )
 } else if ( 300 >= electricityUnit && 201 >= electricityUnit ) {
     unit = electricityUnit
+    remaningUnit = unit - 200
+    electiryBill = remaningUnit * 15  + ( 100 * 13 ) + ( 100 * 10 )
 } else if ( 200 >= electricityUnit && 101 >= electricityUnit ) {
     unit = electricityUnit
+    remaningUnit = unit - 100
+    electiryBill = remaningUnit *  13  + ( 100 * 10 )
 } else if ( 100 >= electricityUnit ) {
     unit = electricityUnit
+    remaningUnit = unit - 100
+    electiryBill = remaningUnit *  10 
 } else {
-    console.log("WRONG INPUT")
+    console.log(`WRONG INPUT`)
 }
+
+console.log(`Your Bill of unit is : ${unit} and your bill is : ${electiryBill} `)
 
 
 
