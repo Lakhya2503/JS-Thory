e5037e29373ba5d3ec2142ee766eeab8ede0dc8e
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-11 14:36:25 +0530

    Fix variable name typo and improve electricity bill calculation logic

--- DSA/02_Basic/02.js
+++ DSA/02_Basic/02.js
@@ -6,39 +6,38 @@
 // 400 above -> 20rs/unit
 
 
-let electricityUnit;
-let electiryBill;
+let electricityUnit = 400;
+let electricityBill;
 let unit = 0;
 let remaningUnit;
 let perUnitRate = 0;
 
 
-if(400 < electricityUnit) {
+if(400 <= electricityUnit) {
     unit = electricityUnit
     remaningUnit = unit - 400
-    electiryBill = remaningUnit * 20 + ( 100 * 18 ) + ( 100 * 15 ) + ( 100 * 13 ) + ( 100 * 10 )
-} else if ( 400 >= electricityUnit && 301 >= electricityUnit ) {
+    electricityBill = remaningUnit * 20 + ( 100 * 18 ) + ( 100 * 15 ) + ( 100 * 13 ) + ( 100 * 10 )
+} else if ( 399 >= electricityUnit && 301 >= electricityUnit ) {
     unit = electricityUnit
     remaningUnit = unit - 300
-    electiryBill = remaningUnit * 18 + ( 100 * 15 ) + ( 100 * 13 ) + ( 100 * 10 )
+    electricityBill = remaningUnit * 18 + ( 100 * 15 ) + ( 100 * 13 ) + ( 100 * 10 )
 } else if ( 300 >= electricityUnit && 201 >= electricityUnit ) {
     unit = electricityUnit
     remaningUnit = unit - 200
-    electiryBill = remaningUnit * 15  + ( 100 * 13 ) + ( 100 * 10 )
+    electricityBill = remaningUnit * 15  + ( 100 * 13 ) + ( 100 * 10 )
 } else if ( 200 >= electricityUnit && 101 >= electricityUnit ) {
     unit = electricityUnit
     remaningUnit = unit - 100
-    electiryBill = remaningUnit *  13  + ( 100 * 10 )
+    electricityBill = remaningUnit *  13  + ( 100 * 10 )
 } else if ( 100 >= electricityUnit ) {
     unit = electricityUnit
     remaningUnit = unit - 100
-    electiryBill = remaningUnit *  10 
+    electricityBill = remaningUnit *  10 
 } else {
     console.log(`WRONG INPUT`)
 }
 
-console.log(`Your Bill of unit is : ${unit} and your bill is : ${electiryBill} `)
-
+console.log(`Your electricity bill is for ${electricityUnit} units, and your total bill is ₹${electricityBill}.00.`);
 
 
 
