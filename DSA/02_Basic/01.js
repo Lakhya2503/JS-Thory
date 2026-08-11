c3514b0a3843882e703136fa37de85b62c0eb236
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-11 12:03:18 +0530

    Add discount calculation logic with console output for various price ranges

---
+++ DSA/02_Basic/01.js
@@ -0,0 +1,42 @@
+// discount counter
+// 0rs to 1000rs -> 0% discount
+// 1001rs to 5000rs -> 5% discount
+// 5001rs to 9000rs -> 10% discount
+// 9001rs to 10000rs -> 20% discount
+// 10001rs above 25% discount
+/*
+let amount = 120000;
+let paybelAmount;
+let discount;
+let discountAmount;
+
+if(0 >= amount && amount <= 1000) {
+    discount = 0
+    discountAmount = (discount * amount) / 100
+    paybelAmount = Math.floor(amount - discountAmount)
+    console.log(`A ${discount}% discount will be applied to your amount of ₹${amount}.00. Your final payable amount is ₹${paybelAmount}.00.`);
+} else if(1001 >= amount && amount <= 5000) {
+    discount = 5
+    discountAmount = (discount * amount) / 100
+    paybelAmount = Math.floor(amount - discountAmount)
+    console.log(`A ${discount}% discount will be applied to your amount of ₹${amount}.00. Your final payable amount is ₹${paybelAmount}.00.`);
+} else if(5001 >= amount && amount <= 9000) {
+    discount = 10
+    discountAmount = (discount * amount) / 100
+    paybelAmount = Math.floor(amount - discountAmount)
+    console.log(`A ${discount}% discount will be applied to your amount of ₹${amount}.00. Your final payable amount is ₹${paybelAmount}.00.`);
+} else if(9001 >= amount && amount <= 10000) {
+    discount = 20
+    discountAmount = (discount * amount) / 100
+    paybelAmount = Math.floor(amount - discountAmount)
+    console.log(`A ${discount}% discount will be applied to your amount of ₹${amount}.00. Your final payable amount is ₹${paybelAmount}.00.`);
+}  else if(10001 < amount) {
+    discount = 25
+    discountAmount = (discount * amount) / 100
+    paybelAmount = Math.floor(amount - discountAmount)
+    console.log(`A ${discount}% discount will be applied to your amount of ₹${amount}.00. Your final payable amount is ₹${paybelAmount}.00.`);
+} else {
+    console.log(`Wrong input ... ☹️`)
+}
+
+*/
