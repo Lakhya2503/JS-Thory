b1cc8d4693ae1b7b32e6289c6d234489980a61d7
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-12 15:13:33 +0530

    add loop examples for sum and factorial calculations in 01.js

---
+++ DSA/03_Basic/01.js
@@ -0,0 +1,25 @@
+// loops
+/*
+let num = 4;
+let sum = 0;
+for(let i = 1; i <= num; i++) {
+    sum += i
+    console.log({ sum })
+    console.log({ i })
+}
+
+console.log({ sum })
+*/
+/* ============================================================= */
+/*
+let num = 4;
+let factorial = 1;
+for(let i = 1; i <= num; i++) {
+    factorial *= i
+}
+
+console.log({ factorial })
+
+*/
+
+/* ============================================================= */
