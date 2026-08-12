ac1a2cb1c3906ec773b4ebdd2d1f10292510dd4d
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-12 10:47:32 +0530

    add switch cases examples to 04.js

--- DSA/02_Basic/04.js
+++ DSA/02_Basic/04.js
@@ -18,6 +18,44 @@ let num = 0;
 
 console.log(num > 0 ? "postive" : num < 0 ? "negative" : "zero")
 */
+
 /* =================================================================== */
 
 // Switch Cases
+/*
+let day = 5;
+
+switch(day){
+  case 1: {
+    console.log("Monday")
+    break;
+  }
+  case 2: {
+    console.log("Tuesday")
+    break;
+  }
+  case 3: {
+    console.log("Wensday")
+    break;
+  }
+  case 4: {
+    console.log("Thursday")
+    break;
+  }
+  case 5: {
+    console.log("Friday")
+    break;
+  }
+  case 6: {
+    console.log("Saturday")
+    break;
+  }
+  case 7: {
+    console.log("Sunday")
+    break;
+  }
+}
+
+*/
+
+/* =================================================================== */
