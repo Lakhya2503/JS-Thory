71e1ac7e16b0d50796ba6c9d917c4ccf60601c07
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-12 08:39:52 +0530

    add ternimary operator method on two examples 04.js

---
+++ DSA/02_Basic/04.js
@@ -0,0 +1,13 @@
+// Ternary Operator
+
+/**
+ * ?  :
+ * 
+ * 
+*/  
+// ex .
+/*
+144 > 42 ? console.log("condition true") : console.log("condition false")
+console.log(14 > 42 ? "condition true" : "condition false")
+*/
+
