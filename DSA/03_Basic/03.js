723be4b52126086b6598b200ba842ee5a7a8044e
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-13 18:58:33 +0530

    refactor 03.js to enhance number pyramid logic and update prompt structure

--- DSA/03_Basic/03.js
+++ DSA/03_Basic/03.js
@@ -3,7 +3,12 @@ process.stdout.write("laxman ")
 process.stdout.write("shinde")
 */
 
-/* ================================ */
+/* =========================================================== */
+
+let prompt = require("prompt-sync")();
+let num = prompt("Enter a Number : ");
+
+/* =========================================================== */
 
 //  + + + + 
 //  + + + + 
@@ -11,16 +16,27 @@ process.stdout.write("shinde")
 //  + + + + 
 
 /*
-let prompt = require("prompt-sync")();
-
-let num = prompt("Enter a Number : ");
-
-for(let i = 1; i <= num; i++) {
-    for (let j = 1; j <= num; j++) {
-        process.stdout.write(' * ')
+for(let i = 1; i <= num; i++) { //rows
+    for (let j = 1; j <= num; j++) { // columms
+        process.stdout.write(' + ')
     }
     console.log()
 }
 */
 
+/* =========================================================== */
+
+//  +
+//  + +
+//  + + +
+//  + + + +
+
+/*
+for( let i = 0; i <= num; i++ ) { // rows
+    for( let j = 0; j <= i; j++ ) { // columns
+        process.stdout.write(' + ');
+    }   
+    console.log();
+}
+*/
 /* =========================================================== */
