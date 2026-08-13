6e0e5184be706e7401c3363c48da5f7063e54d57
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-13 11:17:48 +0530

    add user input functionality to 03.js for number entry

--- DSA/03_Basic/03.js
+++ DSA/03_Basic/03.js
@@ -3,3 +3,9 @@ process.stdout.write("laxman ")
 process.stdout.write("shinde")
 */
 
+/* ================================ */
+
+let prompt = require("prompt-sync")();
+
+let a = prompt("Enter a Number : ");
+process.stdout.write(a)
