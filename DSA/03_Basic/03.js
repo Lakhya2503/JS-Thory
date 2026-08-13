11ea0bc6569ae366041c7f738227210c6baba3a2
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-13 11:52:35 +0530

    implement number pyramid display based on user input in 03.js

--- DSA/03_Basic/03.js
+++ DSA/03_Basic/03.js
@@ -7,5 +7,12 @@ process.stdout.write("shinde")
 
 let prompt = require("prompt-sync")();
 
-let a = prompt("Enter a Number : ");
-process.stdout.write(a)
+let num = prompt("Enter a Number : ");
+
+for(let i = 1; i <= num; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write(' u ')
+    }
+    console.log()
+}
+console.log()
