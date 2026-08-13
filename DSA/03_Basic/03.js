d2e5c389f1cb466d52b0ba3f6218e1100f843053
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-13 18:38:24 +0530

    refactor 03.js to adjust number pyramid display logic and clean up commented sections

--- DSA/03_Basic/03.js
+++ DSA/03_Basic/03.js
@@ -5,14 +5,22 @@ process.stdout.write("shinde")
 
 /* ================================ */
 
+//  + + + + 
+//  + + + + 
+//  + + + + 
+//  + + + + 
+
+/*
 let prompt = require("prompt-sync")();
 
 let num = prompt("Enter a Number : ");
 
 for(let i = 1; i <= num; i++) {
-    for (let j = 1; j <= i; j++) {
-        process.stdout.write(' u ')
+    for (let j = 1; j <= num; j++) {
+        process.stdout.write(' * ')
     }
     console.log()
 }
-console.log()
+*/
+
+/* =========================================================== */
