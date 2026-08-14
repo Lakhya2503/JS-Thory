ebf563e746a1d236965cdfc2cf3f0c4d6b842e9b
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-14 16:26:07 +0530

    refactor 03.js to add reversed array logic and clean up commented sections

---
+++ DSA/04_Basic/03.js
@@ -0,0 +1,15 @@
+// Revers Array
+/*
+let array = [ 1, 2, 3, 4, 5]
+let newArray = new Array(array.length)
+
+let j = 0
+
+for (let i = array.length - 1; i >= 0; i--) {
+    newArray[j] = array[i]
+    j++
+}
+
+console.log({ newArray })
+*/
+/* ======================================================================== */
