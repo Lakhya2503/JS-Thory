7a6f78e8b568aebab3ba17b632cd1e135036c296
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-14 16:51:19 +0530

    refactor 03.js to clarify section titles and improve formatting of array reversal examples

--- DSA/04_Basic/03.js
+++ DSA/04_Basic/03.js
@@ -1,4 +1,4 @@
-// Revers Array
+// Revers Array part 1
 /*
 let array = [ 1, 2, 3, 4, 5]
 let newArray = new Array(array.length)
@@ -12,4 +12,25 @@ for (let i = array.length - 1; i >= 0; i--) {
 
 console.log({ newArray })
 */
+/* ======================================================================== */
+
+// Revers Array part 2
+// means no extra array created
+
+/*
+let array = [ 1, 2, 3, 4, 5]
+let i = 0;
+let j = array.length - 1;
+
+while( i !== j ) {
+    let tempopraryArray = array[i]
+    array[i] = array[j]
+    array[j] = tempopraryArray
+    i++;
+    j--;
+}
+
+console.log({ array })
+*/
+
 /* ======================================================================== */
