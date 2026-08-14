1a753444ee1106562858a10daa0f32db1ebc5f82
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-14 15:44:25 +0530

    refactor 02.js to correct comments and improve clarity of array usage

--- DSA/04_Basic/02.js
+++ DSA/04_Basic/02.js
@@ -1,4 +1,4 @@
-// max element
+// ARRAY
 
 // find larget numnber
 
@@ -20,3 +20,23 @@ console.log({maxNum})
 
 /* ============================================================== */
 
+// find second larget numnber
+/*
+let arr = [85, 42, 87, 1, 244, 99]
+let maxNum = Math.max(arr[0], arr[1])
+let sMaxNum = Math.max(arr[0], arr[1])
+
+// why start loop from the 2 ..?? 
+//  ** Beacuse the 2 position is already checked first declarations
+for( let i = 2; i < arr.length; i++ ) { 
+    if( maxNum < arr[i] ) {
+        maxNum = arr[i]
+    } else if ( sMaxNum < arr[i] && maxNum !== arr[i] ) {
+        sMaxNum = arr[i]
+    }
+}
+
+console.log({sMaxNum})
+console.log({maxNum})
+*/
+/* ============================================================== */
