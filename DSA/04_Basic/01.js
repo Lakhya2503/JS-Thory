718640e6c1b07ca4ef90b05a947d439526171a83
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-14 14:01:47 +0530

    refactor 01.js to improve comments and clarify array usage examples

--- DSA/04_Basic/01.js
+++ DSA/04_Basic/01.js
@@ -1,5 +1,7 @@
 // Arrays
 
+// arrays is dynamic in js
+
 // what your wan't to store on collection like
 // ex 
 
@@ -9,4 +11,36 @@
 */
 
 /** =================================================== */
+
+/*
+myEx = []
+myEx[3] = "Rajesh"
+myEx[2] = "Harshal"
+myEx[9] = "Niketan"
+  console.log({myEx})
+*/
+
+/** output like this but what is the 2 empty 5 empty -> empty is sign of your index are be empty and also 
+ * empty is also a value which is appear
+ * myEx: [ <2 empty items>, 'Harshal', 'Rajesh', <5 empty items>, 'Niketan' ]
+ */
+
+
+/** =================================================== */
+/*
+let prompt = require("prompt-sync")();
+
+
+let myExBf = new Array(5)
+for (let i = 0; i < myExBf.length; i++) {
+    let bfNames = prompt(`Enter a ${i} bf  Name : `);
+    myExBf[i] = bfNames
+}
+
+myExBf.length <= 3 ? console.log("Ex was Snake 🐍") : console.log("Ex was cloune 🤡")
+
+console.log({myExBf})
+*/
+/* ======================================================================== */
+
 
