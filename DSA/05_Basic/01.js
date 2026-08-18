02b4401c318429ca0fa762235361200298bf8057
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-18 10:28:35 +0530

    refactor 01.js to uncomment array reversal logic and improve code clarity

---
+++ DSA/05_Basic/01.js
@@ -0,0 +1,19 @@
+// left revers the array
+/*
+let myArray = [ 1, 2, 3, 4, 5, 6, 7 ];
+
+let copyIndex = myArray[0];
+
+for(let i = 0; i < myArray.length - 1; i++) {
+    myArray[i] = myArray[i + 1] // output expected
+}
+
+myArray[myArray.length - 1] = copyIndex
+
+console.log( myArray )
+
+*/
+
+/ * =============================================================== * /
+
+
