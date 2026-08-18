96b33e6473934ecd887cc4e2a23bc6ddff0dd515
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-18 14:19:50 +0530

    refactor 01.js to correct comments for left rotation and improve clarity

--- DSA/05_Basic/01.js
+++ DSA/05_Basic/01.js
@@ -1,4 +1,4 @@
-// left revers the array
+// left roation of 1 
 /*
 let myArray = [ 1, 2, 3, 4, 5, 6, 7 ];
 
@@ -16,4 +16,22 @@ console.log( myArray )
 
 / * =============================================================== * /
 
+
+// right rotation of 1
+/*
+let myArray = [ 1, 2, 3, 4, 5, 6, 7 ];
+
+let copyIndex = myArray[myArray.length - 1];
+
+for ( let i = myArray.length - 1 ; i > 0; i --) {
+    myArray[i] = myArray[i-1]
+}
+
+myArray[0] = copyIndex
+
+console.log({ myArray })
+
+*/
+
+/ * =============================================================== * /
 
